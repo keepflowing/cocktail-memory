a9264ba1c2ee12f48860adc5993bf3830e1ed5e8
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-11-19 22:30:26 -0500

    Write test to connect to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+async function getCocktail() {
+  const fullUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+  const result = await fetch(fullUrl, {mode: 'cors'});
+  const data = await result.json();
+  return data;
+}
+
+const drinkList = []
+
+for (let i = 0; i < 5; i++) {
+  const result = await getCocktail()
+  drinkList.push(result.drinks[0].strDrink)
+}
+
 function App() {
+
+  // console.log(drinkList)
 
   return (
     <>
-      <h1>Hello World</h1>
+      {drinkList.map(drink => <h1 key={drink}>{drink}</h1>)}
     </>
   )
 }
