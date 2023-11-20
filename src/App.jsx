cac69ed3ca4dad92dc66b10e70abb287fce00f12
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-11-20 18:45:05 -0500

    Add tracker for clicked Id's, shuffles card after every click

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import { useState, useEffect } from "react";
 
-function List({drinks, num}) {
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function List({drinks, num, handleClick}) {
   const arr = []
   for (let i = 0; i < num; i++) {
     arr.push(i)
@@ -11,15 +29,20 @@ function List({drinks, num}) {
       {arr.map(n => 
         <Card 
           key={n} 
+          drinkId={drinks[n] && drinks[n].idDrink}
+          handleClick={handleClick}
           name={drinks[n] ? drinks[n].strDrink : 'Loading...'}
           url={drinks[n] ? drinks[n].strDrinkThumb : 'archerload.gif'}/>)}
     </>
   )
 }
 
-function Card({name, url}) {
+function Card({name, url, drinkId, handleClick}) {
   return (
-    <div className='card'>
+    <div 
+      className='card'
+      onClick={() => handleClick(drinkId)}
+    >
       <h2>{name}</h2>
       <img src={url}/>
     </div>
@@ -27,8 +50,24 @@ function Card({name, url}) {
 }
 
 function App() {
+  const [clicked, setClicked] = useState([])
   const [drinks, setDrinks] = useState([])
   const [num, setNum] = useState(3)
+
+  const handleClick = (id) => {
+    let count = 0;
+    for (let i = 0; i < clicked.length; i++) {
+      if (clicked[i] === id) {
+        count++
+      }
+    }
+    if (count === 0) {
+      setClicked([...clicked, id])
+    } else {
+      setClicked([])
+    }
+    setDrinks(shuffle([...drinks]))
+  }
 
   const handleNumChange = async (num) => {
     const arr = []
@@ -50,8 +89,9 @@ function App() {
   return (
     <>
       <div id='content'>
-       <List drinks={drinks} num={num}/>
+       <List drinks={drinks} num={num} handleClick={handleClick}/>
       </div>
+      <div>{clicked.length}</div>
       <button onClick={() => setNum(num+1)}>New Drinks</button>
     </>
   )
